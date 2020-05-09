a30da81357b55190a99d22292c844cab903178a7
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2020-05-09 12:02:57 +0200

    Rewrite to use hooks

--- src/TimeInput.js
+++ src/TimeInput.js
@@ -1,23 +1,16 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import getNewValue from './utils/getNewValue';
 
-class TimeInput extends PureComponent {
-    state = {
-        value: this.props.initialTime,
-    };
-
-    handleChange = (event) => {
-        const { onChange } = this.props;
-        const { value } = this.state;
-
+// eslint-disable-next-line no-unused-vars
+const TimeInput = ({ initialTime, input, onChange, ...props }) => {
+    const [value, setValue] = useState(initialTime);
+    const handleChange = (event) => {
         const newValue = getNewValue(value, event.target.value);
 
         if (newValue) {
-            this.setState({
-                value: newValue,
-            });
+            setValue(newValue);
 
             if (onChange) {
                 onChange(event, newValue);
@@ -25,29 +18,23 @@ class TimeInput extends PureComponent {
         }
     };
 
-    render () {
-        // eslint-disable-next-line no-unused-vars
-        const { initialTime, input, ...props } = this.props;
-        const { value } = this.state;
-
-        if (input) {
-            return React.cloneElement(input, {
-                ...props,
-                onChange: this.handleChange,
-                value,
-            });
-        }
-
-        return (
-            <input
-                {...props}
-                onChange={this.handleChange}
-                type="text"
-                value={value}
-            />
-        );
+    if (input) {
+        return React.cloneElement(input, {
+            ...props,
+            onChange: handleChange,
+            value,
+        });
     }
-}
+
+    return (
+        <input
+            {...props}
+            onChange={handleChange}
+            type="text"
+            value={value}
+        />
+    );
+};
 
 TimeInput.propTypes = {
     initialTime: PropTypes.string.isRequired,
